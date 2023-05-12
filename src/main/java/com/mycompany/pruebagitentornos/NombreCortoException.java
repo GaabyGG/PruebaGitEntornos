72bce3a0e143d91da2fcb0ba450d3d51706b2984
Gaby <122447991+GaabyGG@users.noreply.github.com>
2023-05-12 12:23:35 +0200

    Update NombreCortoException.java

--- src/main/java/com/mycompany/pruebagitentornos/NombreCortoException.java
+++ src/main/java/com/mycompany/pruebagitentornos/NombreCortoException.java
@@ -10,6 +10,6 @@ package com.mycompany.pruebagitentornos;
  */
 public class NombreCortoException {
     public String verMensaje() {
-        return "El nombre es muy corto, hasta luego";
+        return "El nombre es muy corto";
     }
 }
